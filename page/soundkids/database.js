a639605cb4001554aa9f6052a1dbcf9b7374f4f6
kindycity <kindycity.ict@gmail.com>
2026-06-10 09:03:25 +0700

    kids

---
+++ page/soundkids/database.js
@@ -0,0 +1,71 @@
+// Tệp lưu trữ dữ liệu tự động - Không sửa thủ công
+window.kindergartenData = {
+  "classes": [
+    "Koala",
+    "Panda",
+    "Dolphin",
+    "Giraffe"
+  ],
+  "students": [
+    {
+      "id": "st-1710000001",
+      "name": "Nguyễn Minh Anh",
+      "class": "Koala",
+      "image": "C:\\Users\\Administrator\\Downloads\\HimLam\\HimLam\\app\\src\\main\\assets\\images\\1.kl\\chloe.jpg",
+      "sound": "C:\\Users\\Administrator\\Downloads\\HimLam\\HimLam\\app\\src\\main\\assets\\sound\\1.kl\\chloe.mp3",
+      "isBus": true,
+      "busRoute": "1"
+    },
+    {
+      "id": "st-1710000002",
+      "name": "Trần Bảo Nam",
+      "class": "Panda",
+      "image": "C:\\Users\\Administrator\\Downloads\\HimLam\\HimLam\\app\\src\\main\\assets\\images\\1.kl\\lincoln.jpg",
+      "sound": "sound/panda_baonam.mp3"
+    },
+    {
+      "id": "st-1780996163848",
+      "name": "nguyễn thanh như",
+      "class": "Koala",
+      "image": "C:\\Users\\Administrator\\Downloads\\HimLam\\HimLam\\app\\src\\main\\assets\\images\\1.kl\\chloe.jpg",
+      "sound": "B"
+    },
+    {
+      "id": "st-1780996781243",
+      "name": "nguyen van a",
+      "class": "Koala",
+      "image": "C:\\Users\\Administrator\\Downloads\\HimLam\\HimLam\\app\\src\\main\\assets\\images\\1.kl\\chloe.jpg",
+      "sound": "b.b.bb"
+    },
+    {
+      "id": "st-1780998050745",
+      "name": "nguyen van b",
+      "class": "Giraffe",
+      "image": "gggg",
+      "sound": "ggggga"
+    },
+    {
+      "id": "st-1781000247804",
+      "name": "Thái Duy Bảo",
+      "class": "Dolphin",
+      "image": "chó",
+      "sound": "bảo"
+    },
+    {
+      "id": "st-1781000338298",
+      "name": "Trần Thị Thu Hà",
+      "class": "Koala",
+      "image": "C:\\Users\\Administrator\\Downloads\\HimLam\\HimLam\\app\\src\\main\\assets\\images\\1.kl\\chloe.jpg",
+      "sound": "Hà"
+    },
+    {
+      "id": "st-1781002932092",
+      "name": "TRẦN THỊ NGỌC DUNG",
+      "class": "Koala",
+      "image": "sdasdasd",
+      "sound": "ádasd",
+      "isBus": false,
+      "busRoute": ""
+    }
+  ]
+};
