4bb031322596ae67b38df03d3f5a964e6956aab9
kindycity <kindycity.ict@gmail.com>
2026-06-10 15:47:51 +0700

    Upload

--- page/soundkids/js/bus.js
+++ page/soundkids/js/bus.js
@@ -3,7 +3,7 @@
 function playClickBus() {
 const sounds = ["sound/bus.mp3", "sound/0.bus/adele.mp3", "sound/0.bus/ben.mp3", "sound/0.bus/bo.mp3", "sound/0.bus/coca.mp3", "sound/0.bus/dminh.mp3", "sound/0.bus/nicky.mp3"];
 const audio = document.getElementById("sound0");
-let delay = 200;
+let delay = 220;
 sounds.forEach(sound => {
 setTimeout(() => {
 audio.src = sound;
